bae99419cc18b38cf0cc05bcd38bb8278e753cf8
Jonathan Whittle <jonathan.m.whittle@gmail.com>
2021-07-07 16:05:26 -0400

    svg heist

---
+++ the-divine-cheese-code/src/the_divine_cheese_code/visualization/svg.clj
@@ -0,0 +1,59 @@
+(ns the-divine-cheese-code.visualization.svg
+  (:require [clojure.string :as s])
+  (:refer-clojure :exclude [min max]))
+
+(defn comparator-over-maps
+  [comparison-fn ks]
+  (fn [maps]
+    (zipmap ks
+            (map (fn [k] (apply comparison-fn (map k maps)))
+                 ks))))
+
+(def min (comparator-over-maps clojure.core/min [:lat :lng]))
+(def max (comparator-over-maps clojure.core/max [:lat :lng]))
+
+(defn latlng->point
+  "Convert lat/long map to comma-separated string"
+  [latlng]
+  (str (:lng latlng) "," (:lat latlng)))
+
+(defn translate-to-OO
+  [locations]
+  (let [mincoords (min locations)]
+    (map #(merge-with - % mincoords) locations)))
+
+(defn scale
+  [width height locations]
+  (let [maxcoords (max locations)
+        ratio {:lat (/ height (:lat maxcoords))
+               :lng (/ width (:lng maxcoords))}]
+    (map #(merge-with * % ratio) locations)))
+
+(defn points
+  [locations]
+  (clojure.string/join " " (map latlng->point locations)))
+
+(defn line
+  [points]
+  (str "<polyline points=\"" points "\" />"))
+
+(defn transform
+  [width height locations]
+  (->> locations
+       translate-to-OO
+       (scale width height)))
+
+(defn xml
+  "svg 'template', which also flips the coordinates system"
+  [width height locations]
+  (str "<svg height=\"" height "\" width=\"" width "\" >"
+       ;; These two <g> tags change the coordinate system so that
+       ;; O,O is in the lower-left corner, instead of SVG's default
+       ;; upper-left corner
+       "<g transform=\"translate(0," height ")\">"
+       "<g transform=\"rotate(-90)\">"
+       (-> (transform width height locations)
+           points
+           line)
+       "</g></g>"
+       "</svg>"))
